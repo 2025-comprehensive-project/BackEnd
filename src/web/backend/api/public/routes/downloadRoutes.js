f0a62900f146eb525678f3a8b9e8abcfbe9dcec8
Choi Jae In <Choijaein0125@gmail.com>
2025-06-08 16:44:37 +0900

    06/08 Public API APK 다운로드 수정
    다운로드 오류 발생 수정

--- src/web/backend/api/public/routes/downloadRoutes.js
+++ src/web/backend/api/public/routes/downloadRoutes.js
@@ -4,12 +4,22 @@ const router = express.Router();
 
 // [GET] /api/public/download/apk
 router.get('/apk', (req, res) => {
-  const filePath = path.resolve(__dirname, '../../../../../apk/flapper-moonshine-v1.0.apk');
-  res.download(filePath, 'flapper-moonshine.apk', (err) => {
+  const filePath = path.resolve(__dirname, '../../../../../apk/FlapperAndMoonshine.apk');
+  res.download(filePath, 'FlapperMoonshine.apk', (err) => {
     if (err) {
+      if (err.code === 'EPIPE') {
+        // 연결 중단된 경우는 조용히 무시
+        return;
+      }
       console.error('❌ 파일 다운로드 실패:', err);
-      res.status(500).send('파일 다운로드 실패');
+      if (!res.headersSent) {
+        res.status(500).send('파일 다운로드 실패');
+      }
     }
+  });
+
+  res.on('close', () => {
+    console.warn('⚠️ 다운로드가 중단되었습니다.');
   });
 });
 
